b3fe793c9d8d529e5fb0317ece11d67a054cb7d2
wjh <wjh2012@naver.com>
2025-07-07 23:15:19 +0900

    init

---
+++ cilium/ciliun-helm.sh
@@ -0,0 +1,41 @@
+kubeadm init --pod-network-cidr=10.0.0.0/16 --skip-phases=addon/kube-proxy
+
+kubectl -n kube-system delete ds kube-proxy
+kubectl -n kube-system delete cm kube-proxy
+iptables-save | grep -v KUBE | iptables-restore
+
+helm repo add cilium https://helm.cilium.io/
+
+API_SERVER_IP=192.168.45.131
+API_SERVER_PORT=6443
+
+helm install cilium cilium/cilium --version 1.18.0-rc.0 \
+  --namespace kube-system \
+  --set kubeProxyReplacement=true \
+  --set k8sServiceHost=${API_SERVER_IP} \
+  --set k8sServicePort=${API_SERVER_PORT} \
+  --set hubble.ui.enabled=true \
+  --set hubble.relay.enabled=true \
+  --set prometheus.enabled=true \
+  --set operator.prometheus.enabled=true \
+  --set hubble.metrics.enableOpenMetrics=true \
+  --set hubble.metrics.enabled="{dns:query;ignoreAAAA,drop,tcp,flow,port-distribution,icmp,httpV2:exemplars=true;labelsContext=source_ip\,source_namespace\,source_workload\,destination_ip\,destination_namespace\,destination_workload\,traffic_direction}"
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_gatewayclasses.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_gateways.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_httproutes.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_referencegrants.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_grpcroutes.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml
+
+
+helm upgrade cilium cilium/cilium --version 1.18.0-rc.0 \
+  --namespace kube-system \
+  --reuse-values \
+  --set kubeProxyReplacement=true \
+  --set gatewayAPI.enabled=true
+
+kubectl -n kube-system rollout restart deployment/cilium-operator
+kubectl -n kube-system rollout restart ds/cilium
+cilium status
