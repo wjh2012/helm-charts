4b092987fa7579a8ee145781c0510d1cdafc83a6
wjh <wjh2012@naver.com>
2025-07-08 13:30:02 +0900

    cilium gateway api nodeport

--- cilium/cilium-helm.sh
+++ cilium/cilium-helm.sh
@@ -39,3 +39,8 @@ helm upgrade cilium cilium/cilium --version 1.18.0-rc.0 \
 kubectl -n kube-system rollout restart deployment/cilium-operator
 kubectl -n kube-system rollout restart ds/cilium
 cilium status
+
+helm upgrade cilium cilium/cilium --version 1.18.0-rc.0 \
+  --namespace kube-system \
+  --reuse-values \
+  --set gatewayAPI.service.type=NodePort
