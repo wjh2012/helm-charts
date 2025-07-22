9f120d7090db9380e10778ee8ebcc98a2af29962
wjh <wjh2012@naver.com>
2025-07-23 00:17:34 +0900

    metallb

--- metallb/metalb-helm.sh
+++ metallb/metalb-helm.sh
@@ -1,3 +1,2 @@
-kubectl create namespace metallb-system
 helm repo add metallb https://metallb.github.io/metallb
-helm install metallb metallb/metallb -n metallb-system -f values.yaml
+helm install metallb metallb/metallb
