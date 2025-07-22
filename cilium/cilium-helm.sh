98d5e35d41ba9650a1895abe2754c3bd4fb5a238
wjh <wjh2012@naver.com>
2025-07-22 22:52:47 +0900

    metallb

--- cilium/cilium-helm.sh
+++ cilium/cilium-helm.sh
@@ -1,32 +1,40 @@
 kubeadm init --pod-network-cidr=10.0.0.0/16 --skip-phases=addon/kube-proxy
 
-kubectl -n kube-system delete ds kube-proxy
-kubectl -n kube-system delete cm kube-proxy
-iptables-save | grep -v KUBE | iptables-restore
-
 helm repo add cilium https://helm.cilium.io/
+helm repo update
 
 API_SERVER_IP=192.168.45.131
 API_SERVER_PORT=6443
 
-helm install cilium cilium/cilium --version 1.18.0-rc.0 \
-  --namespace kube-system \
-  --set kubeProxyReplacement=true \
-  --set gatewayAPI.enabled=true \
-  --set gatewayAPI.service.type=NodePort \
-  --set k8sServiceHost=${API_SERVER_IP} \
-  --set k8sServicePort=${API_SERVER_PORT} \
-  --set hubble.ui.enabled=true \
-  --set hubble.relay.enabled=true \
-  --set prometheus.enabled=true \
-  --set operator.prometheus.enabled=true \
-  --set hubble.metrics.enableOpenMetrics=true \
-  --set hubble.metrics.enabled="{dns:query;ignoreAAAA,drop,tcp,flow,port-distribution,icmp,httpV2:exemplars=true;labelsContext=source_ip\,source_namespace\,source_workload\,destination_ip\,destination_namespace\,destination_workload\,traffic_direction}"
+helm install cilium cilium/cilium \
+    --version 1.17.6 \
+    --namespace kube-system \
+    --set kubeProxyReplacement=true \
+    --set k8sServiceHost=${API_SERVER_IP} \
+    --set k8sServicePort=${API_SERVER_PORT}
+
+kubectl -n kube-system get pods -l k8s-app=cilium  
+
+kubectl -n kube-system exec ds/cilium -- cilium-dbg status | grep KubeProxyReplacement
+
+
+#######
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_gatewayclasses.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_gateways.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_httproutes.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_referencegrants.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.2.0/config/crd/standard/gateway.networking.k8s.io_grpcroutes.yaml
+
+########
+
+helm upgrade cilium cilium/cilium --version 1.17.6 \
+    --namespace kube-system \
+    --reuse-values \
+    --set kubeProxyReplacement=true \
+    --set gatewayAPI.enabled=true
+
+kubectl -n kube-system rollout restart deployment/cilium-operator
+kubectl -n kube-system rollout restart ds/cilium
 
 cilium status
---
+++ metallb/metalb-helm.sh
@@ -0,0 +1,2 @@
+helm repo add metallb https://metallb.github.io/metallb
+helm install metallb metallb/metallb -n metallb-system -f values.yaml
